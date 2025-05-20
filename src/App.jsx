2bd40ed34c9b53d5f221a5881207239bed18f6d9
Nipun Pramodya Jayasinghe <NipunPra@brandix.com>
2025-05-20 14:56:41 +0530

    Fix: Login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Login from "./components/Login";
 import Register from "./components/Register";
+import AdminPage from "./pages/AdminPage";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -38,7 +38,8 @@ const Login = () => {
                     </div>
 
                     <div>
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        {/* <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button> */}
+                        <button type="submit" className="flex w-full justify-center rounded-md text-sm/6 bg-black">Sign in</button>
                     </div>
                 </form>
 
